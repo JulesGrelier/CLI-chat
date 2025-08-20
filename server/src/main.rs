bc02e168a564d59a7869ae44c58bf03c757e17ee
JulesGrelier <julesgrelier@gmail.com>
2025-08-20 20:01:13 +0200

    Mise en place des fonctions de base

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await?;
+
+    loop {
+        let (mut stream, _) = listener.accept().await?;
+        println!("Nouveau client");
+
+        let mut buf = [0;1024];
+        let _ = stream.read(&mut buf).await?;
+
+        println!("{}", String::from_utf8_lossy(&buf));
+
+        let _response204 = b"HTTP/1.1 204 No Content\r\nContent-Length: 0\r\n\r\n";
+        let response200 = b"HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello World!";
+        stream.write_all(response200).await?;
+
+        println!("{}\n\n", "-".repeat(50));
+    }
 }
--- user/src/main.rs
+++ user/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .post("http://127.0.0.1:8000")
+        .header("Content-Type", "text/plain")
+        .body("Hello world")
+        .send()
+        .await?;
+
+    println!("Response : {:?}", response);
+    println!("Body : {}", response.text().await?);
+
+    Ok(())
 }
